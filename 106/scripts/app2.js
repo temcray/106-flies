71da5427ebcca75e6cabffbde3d9c29a4e61de98
Tatiana McCray <ejamison06@gmail.com>
2025-07-22 20:28:52 -0500

    Add files via upload

--- 106/scripts/app2.js
+++ 106/scripts/app2.js
@@ -1,11 +1,13 @@
-function saveTask() {//get the vales
+function saveTask() {
+    
+    //get the vales
     const title = $("#txtTitle").val();
     const desc = $("#txtDescription").val();
     const color = $("#selColor").val();
     const date = $("#selDate").val();
     const status = $("#selStatus").val();
     const budget = $("#numBudget").val();
-
+    
     console.log(title, desc, color, date, status, budget);
 }
 
@@ -34,12 +36,87 @@ function displayTask(task) {
 //use the content of the object to render the list section
 
 {
-    console.log("hello im the saveButton")
+    console.log("hello im the saveButton");
+    $.ajax({
+        type: "post",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function(response)
+        {console.log(response);},
+        error: function(error)
+        {console.log(error);}
+    })
 }
 
-function init() {
+
+
+function testRequest() 
+{
+$.ajax({
+   type: "get", 
+   url: "http://fsdiapi.azurewebsites.net",
+   success: function(response)
+   {
+    console.log(response);
+   },
+   error: function(error)
+   {
+    console.log(error);
+   }
+});
+}
+function loadTask()
+{
+    $.ajax({
+        type: "get",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks",
+        success: function(response)
+        {let dataJson = JSON.parse(response);
+           for(let i=0;i<dataJson.length;i++)
+           {
+           let currentValue = dataJson[i]
+           if (currentValue.createdBy === "Tatiana") {
+            displayTask(currentValue);
+           }
+           } 
+            console.log(response);
+            console.log(dataJson);
+        },
+        error: function (error){
+            console.log(error);
+        }
+    });
+}
+
+$("#deletAll").on("click", function () {
+    $.ajax({
+        type: "get",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks",
+        success: function (response) {
+            const data = JSON.parse(response);
+            const userTasks = data.filter(t => t.name === "Tatiana");
+
+            userTasks.forEach(task => {
+                $.ajax({
+                    type: "DELETE",
+                    url: `"http://fsdiapi.azurewebsites.net/api/tasks"}/${task._id}`,
+                    success: loadTask,
+                    error: function (error) {
+                        console.log("Delete error:", error);
+                    }
+                });
+            });
+        }
+    });
+});
+
+
+function init() 
+{
     console.log('hello im the init');
     $("#btnSave").click(saveTask);
+    loadTask();
 }
 
 window.onload = init;// it waits until the css and the html resolved
--- 106/scripts/task.js
+++ 106/scripts/task.js
@@ -7,25 +7,8 @@ class Task {
     this.date = date;
     this.status = status;
     this.budget = budget;
-    }
-}
-
-let xhr = new XMLHttpRequest();
-xhr.open("GET", "https://jsonplaceholder.tpicode.com/post");
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        let data = JSON.parse(xhr.responseText);
-        console.log(data);
-    } else {
-       console.error("Request failed. Status:", xhr.status); 
+    this.name = "Tatiana";
     }
 };
 
-xhr.onerror = function () {
-    console.error("Request failed.");
 
-};
-
-
-xhr.send();
